db69d8f44d811355ff3018ea43fcb8f95e924b42
Akplah <assoumatine@gmail.com>
2022-11-18 03:21:02 +0100

    Mise à jour de la méthode UpdateQuality et implémentation des tests jest et mocha.

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,46 +19,50 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+      if (this.items[i].name == 'Sulfuras'){
+        this.items[i].quality = 80 //Pour le Sulfura, la qualité reste égale à 80
+      }
+      else {
+        this.items[i].sellIn -= 1 //On soustrait 1 afin de prendre en compte la fin de la journée 
+
+        if (this.items[i].name == 'Backstage passes' || this.items[i].name == 'Aged Brie') {
+          // Ces 2 produits augmentent en qualité avec le temps 
+          this.items[i].quality = (this.items[i].quality + 1 > 49) ? 50 : (this.items[i].quality + 1)
+          
+          //Le produit Backstage augmente d'avantage en qualité selon les critères
+          if (this.items[i].name == 'Backstage passes'){
+            
+            // Après le concert, il ne reste plus de jours. la qualité passe à 0
+            if (this.items[i].sellIn < 0){
+              this.items[i].quality = 0 
+            }
+            // S'il reste 5 jours ou moins, la qualité augmente de 3. J'ajoute ici 2 car j'avais déja ajouté 1 sur la ligne 30
+            else if (this.items[i].sellIn <= 5){
+              this.items[i].quality = (this.items[i].quality + 2 > 49) ? 50 : (this.items[i].quality + 2)
+            }
+            // S'il reste 10 jours ou moins, la qualité augmente de 2. J'ajoute ici 1 car j'avais déja ajouté 1 sur la ligne 30
+            else if (this.items[i].sellIn <= 10){
+              this.items[i].quality = (this.items[i].quality + 1 > 49) ? 50 : (this.items[i].quality + 1)
+            }
           }
         }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
+
+        //Après la péremption, la qualité se dégrade 2 fois plus vite. Donc pour Conjured, ce sera 4 fois plus vite. 
+        else if(this.items[i].sellIn < 0){
+          switch (this.items[i].name) {
+            case 'Conjured': this.items[i].quality = (this.items[i].quality - 4 < 0) ? 0 : (this.items[i].quality - 4)
+              break;
+            default: this.items[i].quality = (this.items[i].quality - 2 < 0) ? 0 : (this.items[i].quality - 2)
+              break;
           }
         }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
+        //La qualité des produits normaux se dégrade par jour. Pour Conjured, ça va 2 fois plus vite. 
+        else {
+          switch (this.items[i].name) {
+            case 'Conjured': this.items[i].quality = (this.items[i].quality - 2 < 0) ? 0 : (this.items[i].quality - 2)
+              break;
+            default: this.items[i].quality = (this.items[i].quality - 1 < 0) ? 0 : (this.items[i].quality - 1)
+              break;
           }
         }
       }
--- TypeScript/test/mocha/gilded-rose.spec.ts
+++ TypeScript/test/mocha/gilded-rose.spec.ts
@@ -2,9 +2,15 @@ import { expect } from 'chai';
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('Mon test', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras', 0, 0), new Item('Backstage passes', 30, 5), new Item('Aged Brie', 9, 10), 
+    new Item('Backstage passes', 10, 10), new Item('Backstage passes', 5, 10), new Item('Backstage passes', -1, 10),
+    new Item('Conjured', 9, 10), new Item('Conjured', -1, 10), new Item('Normal', 9, 10), new Item('Normal', -1, 10)]);
+    
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+
+    expect(items).to.eql([new Item('Sulfuras', 0, 80), new Item('Backstage passes', 29, 6), new Item('Aged Brie', 8, 11), 
+    new Item('Backstage passes', 9, 12), new Item('Backstage passes', 4, 13), new Item('Backstage passes', -2, 0),
+    new Item('Conjured', 8, 8), new Item('Conjured', -2, 6), new Item('Normal', 8, 9), new Item('Normal', -2, 8)]);
   });
 });
